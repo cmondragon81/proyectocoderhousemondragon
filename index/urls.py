20456250ed3f885f96b3db7a81b40e8c5a0deb3e
cristian <cristianjosemondragon@gmail.com>
2022-03-28 17:44:23 -0300

    haciendo el login y logout

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,11 @@
+from cgitb import html
+from re import template
 from django.urls import path
-from .views import index
+from .views import index, home, logout
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('home/',home,name='home'),
+    path('logout/',LogoutView.as_view(template_name='salida_sist.html'),name='logout')
 ]
--- index/views.py
+++ index/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login as django_login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
 def index(request):
-    return render (request, 'index.html', {})
+    
+
+    if request.method=="POST":
+        form=AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+                django_login(request, user=user)
+                return render(request,'home.html',{})
+            else:
+                return render(request,'index.html',{'form':form})
+
+        else:
+            return render (request,'index.html',{'form':form, 'msg':'Error de autenticacion'})
+    else:
+# django_login, authenticate, AuthenticationForm
+        form=AuthenticationForm()
+        return render (request, 'index.html', {'form':form, 'msg':'Error de login'})
+
+def home(request):
+    return render (request, 'home.html', {})
+
+# def logout(request):
+#     return redirect('index.html',{})
